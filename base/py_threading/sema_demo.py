3dfd1324ffdc2ccc987c433881332b2ba98f184d
kigane <huwkigane@outlook.com>
2023-04-08 21:58:33 +0800

    4.8

---
+++ base/py_threading/sema_demo.py
@@ -0,0 +1,22 @@
+import threading
+
+# Worker thread
+
+
+def worker(n, sema):
+    # Wait to be signaled
+    sema.acquire()
+
+    # Do some work
+    print('Working', n)
+
+
+# Create some threads
+sema = threading.Semaphore(0)
+nworkers = 10
+for n in range(nworkers):
+    t = threading.Thread(target=worker, args=(n, sema,))
+    t.start()
+
+for i in range(nworkers):
+    sema.release()
